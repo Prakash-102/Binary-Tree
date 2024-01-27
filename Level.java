f94cb6da2d94ce7e6d61df83c04ad82e00a04cff
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-27 19:24:50 +0530

    Add files via upload

--- Level.java
+++ Level.java
@@ -1,5 +1,5 @@
 
-//Level of Binary Tree:-
+//Kth Level:-
 import java.util.*;
 
 public class Level {
@@ -16,62 +16,79 @@ public class Level {
         }
     }
 
-    static class BinaryTree {
-        static int idx = -1;
+    static class Info {
+        Node node;
+        int hd;
 
-        public static Node buildTree(int nodes[]) {
-            idx++;
-            if (nodes[idx] == -1) {
-                return null;
-            }
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-
-            return newNode;
+        public Info(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
         }
+    }
 
-        public static void Levelorder(Node root) {
-            if (root == null) {
-                return;
-            }
-            Queue<Node> q = new LinkedList<>();
-            q.add(root);
-            q.add(null);
-            while (!q.isEmpty()) {
-                Node currNode = q.remove();
-                if (currNode == null) {
-                    System.out.println();
-                    if (q.isEmpty()) {
-                        break;
-                    } else {
-                        q.add(null);
-                    }
+    public static void topview(Node root) {
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min = 0, max = 0;
+        q.add(new Info(root, 0));
+        q.add(null);
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+            if (curr == null) {
+                if (q.isEmpty()) {
+                    break;
                 } else {
-                    System.out.print(currNode.data + " ");
-                    if (currNode.left != null) {
-                        q.add(currNode.left);
-                    }
-                    if (currNode.right != null) {
-                        q.add(currNode.right);
-                    }
+                    q.add(null);
+                }
+            } else {
+                if (!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+                }
+                if (curr.node.left != null) {
+                    q.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
                 }
             }
         }
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
 
+    public static void Klevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.println(root.data + " ");
+            return;
+        }
+        Klevel(root.left, level + 1, k);
+        Klevel(root.right, level + 1, k);
     }
 
     public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
-        tree.Levelorder(root);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 2;
+
+        Klevel(root, 1, k);
     }
 }
 
 /*
  * Output:-
- * 1
- * 2 3
- * 4 5 6
+ * 2
+ * 3
  */
